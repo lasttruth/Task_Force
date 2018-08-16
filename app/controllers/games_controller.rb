023c60a246155e49fec2e4e4fd18533e15daac5d
Robin Doirin <robindoirin@gmail.com>
2018-08-16 15:24:18 +0000

    fisrt templates

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,29 @@
 class GamesController < ApplicationController
 
   def index
+    @games = Game.all
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  def new
 
   end
 
+  def create
+    #render plain: params[:game].inspect
+    @game = Game.new(game_params)
+    @game.save
+
+    redirect_to @game
+  end
+
+
+private
+
+  def game_params
+    params.require(:game).permit(:title, :genre, :platform)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'games#index'
+  root 'games#index', as: 'home'
 
-  get 'about' => 'gamesowned#about'
+  get 'about' => 'gamesowned#about', as: 'about'
+  resources :games
 end

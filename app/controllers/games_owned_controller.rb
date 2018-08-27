aada4624e1450c17b4134d11dee3a67504e56892
Robin Doirin <robindoirin@gmail.com>
2018-08-27 00:34:23 +0000

    adding users and sessions

--- app/controllers/games_owned_controller.rb
+++
@@ -1,7 +0,0 @@
-class GamesOwnedController < ApplicationController
-
-  def about
-    @title = 'List of games I own'
-    @content = "woah I own 100 games on steam"
-  end
-end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+  def show
+    if current_user
+      @user = User.new
+    else
+      redirect_to sign_in_path
+    end
+ end
+
+ def new
+   @user = User.new
+ end
+
+ def create
+   @user = User.new(user_params)
+   if @user.save
+     session[:user] = @user
+     redirect_to user_path(@user)
+   else
+     render 'new'
+   end
+ end
+
+ private
+
+ def user_params
+   params.require(:user).permit(:email, :password)
+ end
 end
